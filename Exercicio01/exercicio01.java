a138a74212e0b4645878c8a6e7e421f57bd8e34e
Daniel <kr.danielreis@gmail.com>
2022-01-18 00:21:17 -0300

    Exercicios

---
+++ Exercicio01/exercicio01.java
@@ -0,0 +1,15 @@
+package com.SpringBoot1.SpringBoot1;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
